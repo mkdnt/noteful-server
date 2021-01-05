9338d1a2c6d36d0ea9bf9f0cd4015e01678acc84
Mike Dent <mikedent85@gmail.com>
2021-01-05 14:42:57 -0500

    postgrator changes

--- src/config.js
+++ src/config.js
@@ -7,4 +7,6 @@ module.exports = {
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://MikeDent@localhost/noteful-test",
+  CLIENT_ORIGIN:
+    process.env.CLIENT_ORIGIN || "https://noteful-beta-bice.vercel.app/",
 };
