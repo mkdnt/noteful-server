e23b8f17173831963fd28be76e5490527d4babbd
Mike Dent <mikedent85@gmail.com>
2021-01-06 14:20:40 -0500

    knexfile and procfile

--- src/app.js
+++ src/app.js
@@ -1,41 +1,34 @@
-
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 const { CLIENT_ORIGIN } = require("./config");
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const foldersRouter = require('./folders/folders-router')
-const notesRouter = require('./notes/notes-router')
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const foldersRouter = require("./folders/folders-router");
+const notesRouter = require("./notes/notes-router");
 
-const app = express()
+const app = express();
 
-const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common' ;
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors({ origin: CLIENT_ORIGIN }))
-app.use(express.json())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors({ origin: CLIENT_ORIGIN }));
+app.use(express.json());
 
-app.use('/folders', foldersRouter)
-app.use('/notes', notesRouter) 
-
-app.get('/', (req, res) => {
-    res.send('Yo, yo...')
-})
+app.use("/folders", foldersRouter);
+app.use("/notes", notesRouter);
 
 app.use(function errorHandler(error, req, res, next) {
-    let response 
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
-module.exports = app
+module.exports = app;
