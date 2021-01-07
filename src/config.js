a914985633729a1fce7d80ddc6fb191ec6a51957
Mike Dent <mikedent85@gmail.com>
2021-01-07 12:20:57 -0500

    resolving SSL errors

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 9000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DATABASE_URL:
-    process.env.DATABASE_URL ||
-    "postgres://postgres://ejneonudvqaeph:5f33ec996f943c18d7c8a85d57741f3cc07df21d82b5be5d8c08ba2e4a239cb4@ec2-18-232-232-96.compute-1.amazonaws.com:5432/dd17208m5mte5g",
+  DATABASE_URL: process.env.DATABASE_URL,
   TEST_DATABASE_URL:
     process.env.TEST_DATABASE_URL ||
     "postgresql://MikeDent@localhost/noteful-test",
--- test/app.spec.js
+++
@@ -1,9 +0,0 @@
-const app = require('../src/app')
-
-describe('App', () => {
-    it('GET / responds with 200 conatining "Yo, yo..."', () => {
-        return supertest(app)
-            .get('/')
-            .expect(200, 'Yo, yo...')
-    })
-})
